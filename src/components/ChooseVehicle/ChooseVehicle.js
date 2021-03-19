4c84df5d2fe425b381acbe96ec8cc35eb08171ae
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-03-19 20:34:34 +0600

    home done

--- src/components/ChooseVehicle/ChooseVehicle.js
+++ src/components/ChooseVehicle/ChooseVehicle.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './ChooseVehicle.css';
+import {Card, Button,Navbar,NavDropdown,Nav } from 'react-bootstrap';
 
-const ChooseVehicle = () => {
+const ChooseVehicle = (props) => {
+    const {id,name,quantity,cost,image} =props.vehicle
     return (
-        <div>
-            
-        </div>
+        <div className="card container-fluid col-lg-3  mt-5 "  >
+            <div className="vehicle-card">
+            <div ><img className="card-img-top" src={image} alt="Card image cap" /> 
+            </div>
+        
+        <div className="card-body">
+          <h5 style={{textAlign:"center"}} className="card-title">{name}</h5>
+          </div>
+          </div>
+        
+      </div>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Bg from '../../images/Bg.png'
 import './Home.css';
+import vehicleData from '../../data/data.json';
+import ChooseVehicle from '../ChooseVehicle/ChooseVehicle';
  
 const Home = () => {
+    const [vehicles,setVehicles]= useState([]);
+    useEffect(() =>{
+        setVehicles(vehicleData);
+        console.log(vehicleData);
+    })
     return (
        
             
-            <div  style={{marginTop:"1px"}} className="card row">
+            <div className="row home" >
+                    <img className="card-img-bottom" src={Bg} alt="Card image cap" />
+                   
         
-        <img className="card-img-bottom" src={Bg} alt="Card image cap" />
+        
+        {
+            vehicles.map(vehicle => <ChooseVehicle vehicle={vehicle}></ChooseVehicle>)
+        }
+     
       </div>
          
         
