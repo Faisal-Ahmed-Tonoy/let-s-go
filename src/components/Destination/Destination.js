f1fbb163111adad4de25dbc4db6eb9d719cba0ae
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-03-21 13:58:56 +0600

    commit

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,29 +1,11 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
-
-const Destination = () => {
-    const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmit = data => console.log(data);
-  
-    console.log(watch("example")); 
-  
-    return (
-      
-      <form onSubmit={handleSubmit(onSubmit)}>
-      
-        <input name="example" defaultValue="test" ref={register} />
-        
-    
-        <input name="exampleRequired" ref={register({ required: true })} />
-      
-        {errors.exampleRequired && <span>This field is required</span>}
-        
-        <input type="submit" />
-      </form>
-    );
-   
-        
-    
-};
-
-export default  Destination
+ import React from 'react';
+ 
+ const Destination = () => {
+   return (
+     <div>
+       
+     </div>
+   );
+ };
+ 
+ export default Destination;
